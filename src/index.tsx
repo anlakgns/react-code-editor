81c9d736f9a00b76418c2671be5584344c26e15b
anlakngs <anilakgunes1@gmail.com>
2021-08-27 01:03:57 +0300

    add bundling in browser

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm'
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {unpkgPathPlugin} from './plugins/unpkgPathPlugin'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const ref = useRef<any>()
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const onClick = async () => {
+    
+    // Guard Clause
+    if(!ref.current) {
+      return 
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window'
+      }
+    })
+
+    console.log(result)
+    setCode(result.outputFiles[0].text)
+  };
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    })
+
+  }
+
+  useEffect(()=> {
+    startService()
+  }, [])
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ src/plugins/unpkgPathPlugin.tsx
@@ -0,0 +1,66 @@
+import * as esbuild from 'esbuild-wasm';
+import axios from 'axios'
+export const unpkgPathPlugin = () => {
+  return {
+
+    // we named this for debugging purposes.
+    name: 'unpkg-path-plugin',
+
+    // bu setup, esbuild de 'build' api kullanıldıkça otomatik olarak çalışıcak.
+    setup(build: esbuild.PluginBuild) {
+
+      // onResolve ve onLoad 2 farklı listener
+
+
+      // onResolve : figure out where file is stored
+      // here we override natural default process of esbuild.
+      // returns the file path 
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log('onResolve', args);
+        if(args.path === "index.js") {
+          return { path: args.path, namespace: 'a' };
+        } 
+
+        if(args.path.includes('./' || args.path.includes('../'))) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
+          }
+        }
+        
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`
+        }
+        
+      })
+ 
+      // Don't let it try to load up something on the file system instead.
+      // override the natural way of loading up a file.
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args);
+ 
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+              import message from 'react';
+              console.log(message);
+            `,
+          };
+        } 
+
+        const {data, request} = await axios.get(args.path);
+        return {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
+        }
+
+
+      });
+    },
+  };
+};
+
+// The filter and namespace is totally for filtering. Sometimes we may want to work this plugin in some special files, we can use them for these kind of as a filter.
