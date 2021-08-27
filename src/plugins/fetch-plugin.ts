15e63df2095c980734ebe33aeaccaf4114a9da46
anlakngs <anilakgunes1@gmail.com>
2021-08-27 17:57:31 +0300

    add css bundle feature

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -1,17 +1,16 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios'
-import localForage from 'localforage'
+import axios from 'axios';
+import localForage from 'localforage';
 
 // to minimize the request, we use indexedDB for caching.
 const fileCache = localForage.createInstance({
-  name: 'filecache'
+  name: 'filecache',
 });
 
 export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetchPlugin',
     setup(build: esbuild.PluginBuild) {
-      
       // Don't let it try to load up something on the file system instead.
       // override the natural way of loading up a file.
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -34,9 +33,27 @@ export const fetchPlugin = (inputCode: string) => {
 
         // if not found, make a request.
         const { data, request } = await axios.get(args.path);
+
+        // checker filetype whether it is js or css
+        // in browser we can't create two file, esbuild wants to build 2 file for one js and the other one for css. In this way we somehow trick the esbuild to insert css.
+        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+        const escaped = data
+          .replace(/\n/g, '') // new lines removed.
+          .replace(/"/g, '\\"') // double quotas escaped
+          .replace(/'/g, "\\'") // single quotes escaped
+        const contents =
+          fileType === 'css'
+          ? `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style)
+        `
+          : data;
+
+
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
-          contents: data,
+          contents: contents,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
 
